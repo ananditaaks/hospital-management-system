864b49e17d98cf243e80679b6a795e58cfb765ba
ananditaaks <ananditakantojha@gmail.com>
2026-04-10 20:53:10 +0530

    Delete backendWebFinal directory

--- backendWebFinal/src/main/resources/static/js/text.js
+++
@@ -1,9 +0,0 @@
-let navbar = document.querySelector('.header .navbars');
-
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.add('active');
-}
-
-document.querySelector('#nav-close').onclick = () =>{
-    navbar.classList.remove('active');
-}
