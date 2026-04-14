395beeecece6ffe244197b160292c62c5913fe47
Ananditakant Ojha <ananditakantojha@gmail.com>
2026-04-14 20:02:45 +0530

    refactor: Improved LoginController method naming and readability.

--- src/main/java/com/example/backendwebfinal/controller/LoginController.java
+++ src/main/java/com/example/backendwebfinal/controller/LoginController.java
@@ -3,14 +3,12 @@ package com.example.backendwebfinal.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class LoginController {
 
+    // Load login page
     @GetMapping("/login")
-    public String login(){
+    public String showLoginPage() {
         return "login";
     }
-
-
 }
