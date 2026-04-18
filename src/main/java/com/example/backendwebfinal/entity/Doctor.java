48e9bc249478089b843833c0af4f67251b6c0786
Ananditakant Ojha <ananditakantojha@gmail.com>
2026-04-18 09:01:59 +0530

    feat: implemented field validation and removed boilerplate code in Doctor entity.

--- src/main/java/com/example/backendwebfinal/entity/Doctor.java
+++ src/main/java/com/example/backendwebfinal/entity/Doctor.java
@@ -1,35 +1,49 @@
 package com.example.backendwebfinal.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 import java.util.List;
 
+@Getter
+@Setter
+@ToString
 @Entity
 @Table(name = "doctors")
 public class Doctor {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(name = "first_name")
+    @NotNull(message = "First name is required")
+    @Size(min = 2, max = 50, message = "First name must be 2-50 characters")
     private String firstName;
 
     @Column(name = "last_name")
+    @NotNull(message = "Last name is required")
+    @Size(min = 2, max = 50)
     private String lastName;
 
     @Column(name = "specialty")
+    @NotNull(message = "Specialty is required")
     private String specialty;
 
     @Column(name = "address")
+    @NotNull(message = "Address is required")
     private String address;
 
     @Column(name = "mobile_number")
+    @NotNull(message = "Mobile number is required")
+    @Pattern(regexp = "\\d{10}", message = "Mobile number must be 10 digits")
     private String mobileNo;
 
     @OneToMany(mappedBy = "doctor", cascade = CascadeType.MERGE)
     private List<Patient> patient;
-
 
     public Doctor() {
     }
@@ -40,73 +54,5 @@ public class Doctor {
         this.specialty = specialty;
         this.address = address;
         this.mobileNo = mobileNo;
-    }
-
-    @Override
-    public String toString() {
-        return "Doctor{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", specialty='" + specialty + '\'' +
-                ", address='" + address + '\'' +
-                ", mobileNo='" + mobileNo + '\'' +
-                '}';
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getSpecialty() {
-        return specialty;
-    }
-
-    public void setSpecialty(String specialty) {
-        this.specialty = specialty;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getMobileNo() {
-        return mobileNo;
-    }
-
-    public void setMobileNo(String mobileNo) {
-        this.mobileNo = mobileNo;
-    }
-
-    public List<Patient> getPatient() {
-        return patient;
-    }
-
-    public void setPatient(List<Patient> patient) {
-        this.patient = patient;
     }
 }
