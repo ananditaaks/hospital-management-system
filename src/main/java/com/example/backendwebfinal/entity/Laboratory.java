c79702f36411cf19c003de84ec3be0af9338c646
Ananditakant Ojha <ananditakantojha@gmail.com>
2026-04-22 13:26:50 +0530

    feat: implemented field validation and optimized Laboratory entity design.

--- src/main/java/com/example/backendwebfinal/entity/Laboratory.java
+++ src/main/java/com/example/backendwebfinal/entity/Laboratory.java
@@ -1,77 +1,41 @@
 package com.example.backendwebfinal.entity;
 
-
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 import java.util.List;
-
 
 @Data
 @Entity
 @Table(name = "laboratory")
 public class Laboratory {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-
     @Column(name = "lab_name")
+    @NotNull(message = "Lab name is required")
+    @Size(min = 2, max = 100)
     private String labName;
 
     @Column(name = "address")
+    @NotNull(message = "Address is required")
     private String address;
 
     @Column(name = "phone")
-    private int phoneNo;
+    @NotNull(message = "Phone number is required")
+    @Pattern(regexp = "\\d{10}", message = "Phone must be 10 digits")
+    private String phoneNo;
 
-    public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getLabName() {
-		return labName;
-	}
-
-	public void setLabName(String labName) {
-		this.labName = labName;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public int getPhoneNo() {
-		return phoneNo;
-	}
-
-	public void setPhoneNo(int phoneNo) {
-		this.phoneNo = phoneNo;
-	}
-
-	public List<Donor> getDonor() {
-		return donor;
-	}
-
-	public void setDonor(List<Donor> donor) {
-		this.donor = donor;
-	}
-
-	@OneToMany(cascade = CascadeType.MERGE, mappedBy = "laboratory")
+    @OneToMany(cascade = CascadeType.MERGE, mappedBy = "laboratory")
     private List<Donor> donor;
 
     public Laboratory() {
     }
 
-    public Laboratory(String labName, String address, int phoneNo, List<Donor> donor) {
+    public Laboratory(String labName, String address, String phoneNo, List<Donor> donor) {
         this.labName = labName;
         this.address = address;
         this.phoneNo = phoneNo;
