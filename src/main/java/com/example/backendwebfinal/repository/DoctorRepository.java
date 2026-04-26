3b8d64d711d461fc80a33a02fb37dec821cf2685
Ananditakant Ojha <ananditakantojha@gmail.com>
2026-04-26 21:03:13 +0530

    Added query method to fetch doctors by specialty.

--- src/main/java/com/example/backendwebfinal/repository/DoctorRepository.java
+++ src/main/java/com/example/backendwebfinal/repository/DoctorRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+//doctor 
 @Repository
 public interface DoctorRepository extends CrudRepository<Doctor, Integer> {
 
