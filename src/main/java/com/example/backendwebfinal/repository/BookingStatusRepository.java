28e0d625ebd5ad334ccc484abc6d84cb0b45bf45
Ananditakant Ojha <ananditakantojha@gmail.com>
2026-04-26 20:07:17 +0530

    Added query method to filter appointments by status field

--- src/main/java/com/example/backendwebfinal/repository/BookingStatusRepository.java
+++ src/main/java/com/example/backendwebfinal/repository/BookingStatusRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 import com.example.backendwebfinal.entity.Appointment;
 
 import java.util.List;
-
+//committing it
 @Repository
 public interface BookingStatusRepository extends JpaRepository<Appointment, Integer> {
 
